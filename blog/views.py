28bafcb887dcf8e708684d0bb25621ae113cb657
Muqaibil <mam_770@yahoo.com>
2020-09-27 00:28:12 +0300

    adding views ad link it to urls and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
+
+def post_list(request):
+    all_posts = Post.objects.all()
+    return render(request, 'Post/post_list.html',{'posts':all_posts} )
+    pass
+
+def post_details (request):
+    pass
